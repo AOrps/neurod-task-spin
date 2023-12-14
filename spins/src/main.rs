737003b85bc56fb7608e1c15df0ce377ca9575cb
AOrps <aorbeandrews@gmail.com>
2023-12-14 15:31:33 -0500

    single-threaded tcp request printing web server
    Signed-off-by: AOrps <aorbeandrews@gmail.com>

--- spins/src/main.rs
+++ spins/src/main.rs
@@ -1,3 +1,30 @@
+use std::{
+    io::{prelude::*,BufReader},
+    net::{TcpListener,TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+
+    let listener = TcpListener::bind("127.0.0.1:7100").unwrap();    
+
+    for _stream in listener.incoming() {
+
+        let _stream = _stream.unwrap();
+
+        handle_conn(_stream);
+        println!("Connection established!");
+    }
+
+}
+
+fn handle_conn(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
