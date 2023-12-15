ecc5f6de955f9f02429fa9aad00be514b54ed993
AOrps <aorbeandrews@gmail.com>
2023-12-14 19:08:48 -0500

    port check + auto-increment functionality
    Signed-off-by: AOrps <aorbeandrews@gmail.com>

--- lib/net.go
+++ lib/net.go
@@ -1,31 +1,50 @@
 package lib
 
+/*
+   This file is responsible for adding the functionality for checking if a
+   certain port is able to listen to a tcp connection (and then subsequently
+   close that connection).
+
+
+   v1.0  @AOrps
+*/
+
 import (
 	"strconv"
 	"net"
 	"fmt"
 )
 
+// make a type called Integer that uses Generics to
+// make all integers try to function similiarly
 type Integer interface {
 	int | int8 | int16 | int32 | int64
 }
 
+// CheckPort: checks if the program is enable to
+//            listen on the <n>th port 
+// ex. CheckPort(7100) -> false
+// ex. CheckPort(7101) -> true 
 func CheckPort[T Integer](n T) bool {
-
-	retBool := true
 
 	port := strconv.Itoa(int(n))
 	
+	// this is where the connection attempt happens
 	ln, err := net.Listen("tcp",":"+port)
 
+	// should it result in an error, then it should be
+	// understood that the <n>th port is already in use
 	if err != nil {
 		fmt.Printf("%s not available\n", port)
-		retBool = false
+		return false		
 	}
 
 	if ln.Close() != nil {
  		fmt.Printf("couldn't stop listening on port %s\n", port)
 	}
 
-	return retBool
+	// should the program get to this stage, there are
+	// no errors and the program was able to listen on
+	// the <n>th port as well as close that listener
+	return true
 }
--- serve.go
+++ serve.go
@@ -12,9 +12,9 @@ const (
 	templatePath = "templates/*htmx"
 )
 
+// (Global) port: initial port to serve content on
+// - should this fail, it will increment value
 var port = 7100
-
-// TODO: Test if PORT is available
 
 
 func rootHandle(w http.ResponseWriter, r *http.Request) {
@@ -33,19 +33,22 @@ func dbHandle(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	servePort := strconv.Itoa(port)
-
 	// Assets at static/
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.HandleFunc("/",rootHandle)
 	http.HandleFunc("/db",dbHandle)
-	
-	fmt.Printf("server: http://127.0.0.1:%s\n", servePort)
-	for http.ListenAndServe(":"+servePort,nil) != nil {
+
+	// checks if port is available and valid
+	for (lib.CheckPort(port) == false) && (port < 65536) {
 		port++
-		servePort = strconv.Itoa(port)
 	}
 
+	// converts port number to string (so ListenAndServe)
+	// can use it post conversion
+	servePort := strconv.Itoa(port)
+	
+	fmt.Printf("server: http://127.0.0.1:%s\n", servePort)
+	http.ListenAndServe(":"+servePort,nil)
 }
