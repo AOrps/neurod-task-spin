951f6ba7c70e530a843fc80bb174ad610a6872bc
AOrps <aorbeandrews@gmail.com>
2023-12-18 17:48:16 -0500

    removed route artifacts from todolist testing
    Signed-off-by: AOrps <aorbeandrews@gmail.com>

--- lib/routes.go
+++ lib/routes.go
@@ -14,34 +14,6 @@ import (
 )
 
 var upgrader = websocket.Upgrader{}
-// var todoList []string
-
-// func getCmd(input string) string {
-//     inputArr := strings.Split(input, " ")
-//     return inputArr[0]
-// }
-
-// func getMessage(input string) string {
-//     inputArr := strings.Split(input, " ")
-//     var result string
-//     for i := 1; i < len(inputArr); i++ {
-//         result += inputArr[i]
-//     }
-//     return result
-// }
-
-// func updateTodoList(input string) {
-//     tmpList := todoList
-//     todoList = []string{}
-//     for _, val := range tmpList {
-//         if val == input {
-//             continue
-//         }
-//         todoList = append(todoList, val)
-//     }
-// }
-
-
 
 func rootHandle(w http.ResponseWriter, r *http.Request) {
 	tpl := template.Must(template.ParseGlob(TEMPLATEPATH))
@@ -79,28 +51,7 @@ func wsHandle(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			fmt.Println(err)
 		}
-		
-		// input := string(message)
-		// cmd := getCmd(input)
-		// msg := getMessage(input)
-		
-		// if cmd == "add" {
-        //         todoList = append(todoList, msg)
-        //     } else if cmd == "done" {
-        //         updateTodoList(msg)
-        //     }
-        //     output := "Current Todos: \n"
-        //     for _, todo := range todoList {
-        //         output += "\n - " + todo + "\n"
-        //     }
-        //     output += "\n----------------------------------------"
-        //     message = []byte(output)
-        //     err = conn.WriteMessage(mt, message)
-        //     if err != nil {
-        //         fmt.Println("write failed:", err)
-        //         break
-        //     }
-	
+			
 	}
 }
 
