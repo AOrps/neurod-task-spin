3acc2907d9354ed69c7da67e934c73e63941fe93
AOrps <aorbeandrews@gmail.com>
2023-12-17 23:16:48 -0500

    centering a div and setuping up websockets
    Signed-off-by: AOrps <aorbeandrews@gmail.com>

--- lib/routes.go
+++ lib/routes.go
@@ -9,7 +9,11 @@ import (
 	"fmt"
 	"net/http"
 	"html/template"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{}
 
 func rootHandle(w http.ResponseWriter, r *http.Request) {
 	tpl := template.Must(template.ParseGlob(TEMPLATEPATH))
@@ -23,6 +27,27 @@ func dbHandle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "db here plz")
 }
 
+func wsHandle(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		mt, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		
+	}
+}
+
 func settingsHandle(w http.ResponseWriter, r *http.Request) {
 	tpl := template.Must(template.ParseGlob(TEMPLATEPATH))
 	page := Page{
@@ -31,8 +56,10 @@ func settingsHandle(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "settings", page)
 }
 
+
 func SetupRoutes() {
 	http.HandleFunc("/",rootHandle)
 	http.HandleFunc("/db",dbHandle)
-	http.HandleFunc("/settings",settingsHandle)	
+	http.HandleFunc("/ws",wsHandle)
+	http.HandleFunc("/settings",settingsHandle)
 }
